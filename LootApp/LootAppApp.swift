7dab008891693e0b3709d1e41fa5c19d03f70b1e
Lou RASSAT <lou.rassat@etu.univ-smb.fr>
2024-01-19 10:40:06 +0100

    Initial Commit

---
+++ LootApp/LootAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootAppApp.swift
+//  LootApp
+//
+//  Created by Lou RASSAT on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LootAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
